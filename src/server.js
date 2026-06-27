7ba283666561d1e85b8bec713b1fb8543a37d21a
Anna <annapuchkova1993@gmail.com>
2026-06-27 17:15:39 +0300

    new nodejs-hw

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { pinoHttp } from 'pino-http';
+
+const logger = pinoHttp({});
+
+const app = express();
+app.use(logger);
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,PATCH,POST,DELETE',
+  }),
+);
+
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: `${err.message}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
